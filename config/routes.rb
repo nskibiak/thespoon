5308d7cdbbc5faa627dcbf304f67de0227aaca62
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-09 16:20:10 +0900

    added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   # refactored get 'pages/contact'
   get 'contact', to: 'pages#contact'
 
+  # VERB PATH, to: 'CONTROLLER#ACTION'
+
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
