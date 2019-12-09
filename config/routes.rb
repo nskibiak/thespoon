b7d5ccb60f9f2ccc27953498219b98b1890aebde
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2019-12-09 16:15:01 +0900

    adjusted router to remove pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # refactored get 'pages/about'
+  get 'about', to: 'pages#about'
+  # refactored get 'pages/contact'
+  get 'contact', to: 'pages#contact'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
